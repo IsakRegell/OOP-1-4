b609df23118fe5299b9a71355b7b88b3e2205e41
IsakRegell <Isak.regell@gmail.com>
2024-09-10 09:35:38 +0200

    Add files via upload

---
+++ Arv oop.cs
@@ -0,0 +1,50 @@
+﻿namespace Arv_princip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bird myBird = new Bird();
+            myBird.animalname = "Isak";
+            myBird.featherColor = "gröna";
+            myBird.displayInfo();
+
+            Fish myFish = new Fish();
+            myFish.eyeColor = "Gul";
+            myFish.displayInfo();
+        }
+    }
+    public class Animal
+    {
+        public string animalname;
+
+        public void displayInfo()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+    public class Bird : Animal
+    {
+        public string featherColor;
+
+        public new void displayInfo()
+        {
+            Console.WriteLine($"Fågeln {animalname} flyger med sina {featherColor} vingar och springer inte!");
+        }
+
+    }
+
+    public class Fish : Animal
+    {
+        public string eyeColor;
+        public new void displayInfo()
+        {
+            Console.WriteLine($"Fiskens ögon är {eyeColor}a");
+        }
+            
+    }
+        
+
+}
+
+
---
+++ Inkapsling oop.cs
@@ -0,0 +1,32 @@
+﻿namespace Lektion_1_vecka_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Man!");
+            Fotbollsspelare Fotbollsspelareobj = new Fotbollsspelare();
+            Fotbollsspelareobj.screan();
+        }
+    }
+
+
+    public class Fotbollsspelare
+    {
+        string nameRonaldo = "ronaldo "; //Atribute
+        static int ageRonaldo = 40;
+        string nameMessi = "messi";
+        static int ageMessi = 39;
+        int totalage = ageMessi + ageRonaldo;
+
+
+
+        //Metod där isak skriver
+        public void screan()
+        {
+            Console.WriteLine("Isak skriver " + nameMessi +" "+ ageMessi + " " + totalage);
+        }
+
+
+    }
+}
