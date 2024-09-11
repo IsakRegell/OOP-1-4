291fce41a44c2f4a55ab1d22ff42bab7492669f2
IsakRegell <Isak.regell@gmail.com>
2024-09-11 16:49:30 +0200

    Add files via upload

---
+++ ABSTRAKTION.cs
@@ -0,0 +1,18 @@
+﻿namespace Abstraktion
+{
+   public abstract class Animal
+    {
+        public abstract void AnimalSound();
+    }
+    public class Dog : Animal
+    {
+        public override void AnimalSound()
+        {
+            Console.WriteLine("Wof");
+        }
+    }
+
+
+
+
+}
