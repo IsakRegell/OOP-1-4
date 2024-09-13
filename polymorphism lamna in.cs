188dc3dcb0c972cdc92676bac4a86cb3af3a7920
IsakRegell <Isak.regell@gmail.com>
2024-09-13 09:59:16 +0200

    Add files via upload

---
+++ polymorphism lamna in.cs
@@ -0,0 +1,20 @@
+﻿namespace Polymorphism1
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Veicle veicle = new Veicle();
+            veicle.StartMotor();
+
+            Bil bil = new Bil();
+            bil.StartMotor();
+
+            Motorcykel motorcykel = new Motorcykel();
+            motorcykel.StartMotor();
+        }
+
+
+    }
+
+}
