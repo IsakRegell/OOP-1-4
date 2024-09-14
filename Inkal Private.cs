dcb256d02c21eac6552afc3cefebdc8da059f143
IsakRegell <Isak.regell@gmail.com>
2024-09-14 09:50:41 +0200

    Add files via upload

---
+++ Inkal Private.cs
@@ -0,0 +1,16 @@
+﻿using Lektion_1_vecka_3;
+
+namespace InkapPrivate
+{
+
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Isak");
+        }
+
+    }
+}
